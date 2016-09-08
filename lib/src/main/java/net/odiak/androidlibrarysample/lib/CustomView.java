b6c8a2366e4e019cdd7fd11e924bbadbeb6fa716
Kaido Iwamoto <h6824.kaido@gmail.com>
2016-09-08 17:48:01 +0900

    Add library module

---
+++ lib/src/main/java/net/odiak/androidlibrarysample/lib/CustomView.java
@@ -0,0 +1,16 @@
+package net.odiak.androidlibrarysample.lib;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class CustomView extends View {
+
+    public CustomView(Context context) {
+        this(context, null);
+    }
+
+    public CustomView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
